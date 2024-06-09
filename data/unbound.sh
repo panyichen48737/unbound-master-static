5e2cc92babbf2e17feaf701896a77cf5c1971ed1
panyichen48737 <95898667+panyichen48737@users.noreply.github.com>
2024-06-09 15:48:25 +0800

    Update unbound.sh

--- data/unbound.sh
+++ data/unbound.sh
@@ -73,8 +73,8 @@ server:
   so-reuseport: yes
   infra-cache-numhosts: 10000
   unwanted-reply-threshold: 10000000
-  so-rcvbuf: 3m
-  so-sndbuf: 3m
+  so-rcvbuf: 2m
+  so-sndbuf: 2m
   msg-cache-size: 64m
   key-cache-size: 64m
   neg-cache-size: 64m
