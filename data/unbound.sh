b59d0b59eebb165be61c89f3774aef28d7b3cc21
panyichen48737 <95898667+panyichen48737@users.noreply.github.com>
2024-06-09 12:16:02 +0800

    Update unbound.sh

--- data/unbound.sh
+++ data/unbound.sh
@@ -31,7 +31,7 @@ else
     slabs=4
 fi
 
-if [ ! -f /opt/unbound/etc/unbound/unbound.conf ]; then
+if [ ! -f /usr/local/unbound/etc/unbound/unbound.conf ]; then
     sed \
         -e "s/@MSG_CACHE_SIZE@/${msg_cache_size}/" \
         -e "s/@RR_CACHE_SIZE@/${rr_cache_size}/" \
@@ -115,6 +115,6 @@ cachedb:
 EOT
 fi
 
-mkdir -p /opt/unbound/etc/unbound && \
+mkdir -p /usr/local/unbound/etc/unbound && \
 
-exec /opt/unbound/sbin/unbound -d -c /opt/unbound/etc/unbound/unbound.conf
+exec /usr/local/unbound/sbin/unbound -d -c /usr/local/unbound/etc/unbound/unbound.conf
