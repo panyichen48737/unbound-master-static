158a39305e9a28b4830ea8c6c27b9f7ca261ad78
panyichen48737 <95898667+panyichen48737@users.noreply.github.com>
2024-06-16 09:34:32 +0800

    Update unbound.sh

--- data/unbound.sh
+++ data/unbound.sh
@@ -14,6 +14,7 @@ server:
   log-time-ascii: yes
   use-syslog: no
   verbosity: 1
+  
   interface: 0.0.0.0@5334	
   access-control: 0.0.0.0/0 allow
   do-not-query-localhost: no
@@ -22,6 +23,7 @@ server:
   do-udp: yes
   do-tcp: yes
   do-daemonize: no
+  
   num-threads: 2
   msg-cache-slabs: 4
   rrset-cache-slabs: 4
@@ -39,17 +41,20 @@ server:
   so-reuseport: yes
   infra-cache-numhosts: 10000
   unwanted-reply-threshold: 10000000
+  
   so-rcvbuf: 4m
   so-sndbuf: 4m
   msg-cache-size: 64m
   key-cache-size: 64m
   neg-cache-size: 64m
   rrset-cache-size: 128m
+  
   outgoing-range: 8192
   num-queries-per-thread: 4096
   outgoing-num-tcp: 1024
   incoming-num-tcp: 2048
   jostle-timeout: 300
+  
   cache-min-ttl: 60
   cache-max-ttl: 3600
   cache-max-negative-ttl: 300
@@ -57,27 +62,32 @@ server:
   serve-expired-ttl: 86400
   serve-expired-reply-ttl: 5
   serve-expired-client-timeout: 1800
+  
   serve-expired: yes
   prefetch: yes
   prefetch-key: yes
   max-udp-size: 4096
+  
   edns-buffer-size: 4096
+  
   send-client-subnet: 0.0.0.0/0
   send-client-subnet: ::0/0
   max-client-subnet-ipv4: 24
   max-client-subnet-ipv6: 56
   client-subnet-always-forward: yes
+  
   module-config: "subnetcache cachedb iterator"
   
   forward-zone:
    name: "."
    forward-addr: 172.16.0.5@5335
-
-cachedb:
+  
+  cachedb:
   backend: "redis"
   redis-server-host: 172.16.0.2
   redis-server-port: 6379
   redis-timeout: 100
+  
 EOT
 fi
 
